2414cbb73e728e3c56cc0067fd362160deacd365
xavier <xavier.kress@gmail.com>
2019-09-15 08:40:53 +0200

    add logging before and after bookController class methods execution with AOP

---
+++ src/main/java/com/demospring/xavierkress/spring_initializr/aspects/BookControllerAspect.java
@@ -0,0 +1,23 @@
+package com.demospring.xavierkress.spring_initializr.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class BookControllerAspect {
+
+    @Before(value = "execution(* com.demospring.xavierkress.spring_initializr.controllers.BookController.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        System.out.println("Before method:" + joinPoint.getSignature());
+    }
+
+    @After(value = "execution(* com.demospring.xavierkress.spring_initializr.controllers.BookController.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("After method:" + joinPoint.getSignature());
+    }
+}
+
